e6ff46bb71f1015334ecf75b3d393553be5c66a4
f304646673 <f304646673@gmail.com>
2025-05-15 17:35:26 +0800

    在 `Customer.cs` 和 `Order.cs` 文件中，将 `Id` 属性的定义从可选更改为必需，确保该属性不能为 null。

--- csharp/Lesson/Lesson1/Models/Customer.cs
+++ csharp/Lesson/Lesson1/Models/Customer.cs
@@ -4,7 +4,7 @@
 
     public class Customer
     {
-        public int Id { get; set; }
+        public required int Id { get; set; }
         public required string Name { get; set; }
         public List<Order>? Orders { get; set; }
     }
