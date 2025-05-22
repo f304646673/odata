3cb1afc96ab23ede8e15c1213264f87d7664cef8
f304646673 <f304646673@gmail.com>
2025-05-22 10:04:51 +0800

    添加获取客户名称的 API 方法
    在 `CustomersController.cs` 文件中，新增了 `GetNameValue` 方法。该方法根据客户的 ID 查找客户名称，未找到时返回 404 Not Found，找到时返回客户名称，内容类型为纯文本（"text/plain"）。
    
    更新 OData 模型定义和操作方法
    
    - 添加 `addEnumType` 方法以定义 `Color` 枚举。
    - 移除 `addComplexType`，引入 `AddressAbstract` 和 `PostalAddress`。
    - 修改 `addEntityType`，使用 `PostalAddress` 替代 `DerivedAddress`。
    - 替换 `addAbstractEntityType` 中的 `Peopele` 为 `OrderAbstract`，并添加 `addDerivedAbstractEntityType`。
    - 在 `addNavigationProperty` 中添加可空类型声明。
    - 新增无绑定和有绑定的函数及操作方法。
    - 更新 `GetEdmModel` 方法以整合新功能。

--- csharp/Lesson/Lesson10/Program.cs
+++ csharp/Lesson/Lesson10/Program.cs
@@ -4,6 +4,8 @@ using System.Xml.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 添加枚举类型
 static void addEnumType(EdmModel model)
 {
     EdmEnumType color = new("Lesson10", "Color");
@@ -12,47 +14,41 @@ static void addEnumType(EdmModel model)
     color.AddMember(new EdmEnumMember(color, "Green", new EdmEnumMemberValue(2)));
     model.AddElement(color);
 }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 添加复杂类型
 static void addComplexType(EdmModel model)
-{
-    EdmComplexType employee = new("Lesson10", "Employee");
-    employee.AddStructuralProperty("Id", EdmPrimitiveTypeKind.Int32);
-    employee.AddStructuralProperty("Name", EdmPrimitiveTypeKind.String);
-    employee.AddStructuralProperty("PerfRating", EdmPrimitiveTypeKind.Decimal);
-    model.AddElement(employee);
-}
-
-static void addDerivedComplexType(EdmModel model)
 {
     EdmComplexType address = new("Lesson10", "Address");
     address.AddStructuralProperty("Street", EdmPrimitiveTypeKind.String);
     address.AddStructuralProperty("City", EdmPrimitiveTypeKind.String);
     address.AddStructuralProperty("State", EdmPrimitiveTypeKind.String);
-    address.AddStructuralProperty("ZipCode", EdmPrimitiveTypeKind.String);
+    address.AddStructuralProperty("Country", EdmPrimitiveTypeKind.String);
     model.AddElement(address);
-    EdmComplexType derivedAddress = new("Lesson10", "DerivedAddress", address);
-    derivedAddress.AddStructuralProperty("Country", EdmPrimitiveTypeKind.String);
+}
+
+static void addDerivedComplexType(EdmModel model)
+{
+    EdmComplexType derivedAddress = new("Lesson10", "PostalAddress", baseType : model.FindType("Lesson10.Address") as EdmComplexType);
+    derivedAddress.AddStructuralProperty("PostalCode", EdmPrimitiveTypeKind.String);
     model.AddElement(derivedAddress);
 }
 
 static void addAbstractComplexType(EdmModel model)
 {
-    EdmComplexType person = new("Lesson10", "Person", baseType: null, isAbstract: true);
-    person.AddStructuralProperty("Id", EdmPrimitiveTypeKind.Int32);
-    person.AddStructuralProperty("FirstName", EdmPrimitiveTypeKind.String);
-    person.AddStructuralProperty("LastName", EdmPrimitiveTypeKind.String);
-    model.AddElement(person);
+    EdmComplexType addressAbstract = new("Lesson10", "AddressAbstract", baseType: null, isAbstract: true);
+    model.AddElement(addressAbstract);
 }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 添加实体类型
 static void addEntityType(EdmModel model)
 {
     EdmEntityType customer = new("Lesson10", "Customer");
     customer.AddKeys(customer.AddStructuralProperty("Id", EdmPrimitiveTypeKind.Int32)); 
     customer.AddStructuralProperty("Name", EdmPrimitiveTypeKind.String);
-    var derivedAddressType = model.FindType("Lesson10.DerivedAddress") as IEdmComplexType;
-    if (derivedAddressType != null)
+    var postalAddressType = model.FindType("Lesson10.PostalAddress") as IEdmComplexType;
+    if (postalAddressType != null)
     {
-        customer.AddStructuralProperty("Address", new EdmComplexTypeReference(derivedAddressType, true));
+        customer.AddStructuralProperty("PostalAddress", new EdmComplexTypeReference(postalAddressType, true));
     }
     model.AddElement(customer);
 }
@@ -66,11 +62,19 @@ static void addDerivedEntityType(EdmModel model)
 
 static void addAbstractEntityType(EdmModel model)
 {
-    EdmEntityType peopele = new("Lesson10", "Peopele", baseType: null, isAbstract: true, isOpen: false);
-    peopele.AddKeys(peopele.AddStructuralProperty("Id", EdmPrimitiveTypeKind.Int32));
-    model.AddElement(peopele);
+    EdmEntityType orderAbstract = new("Lesson10", "OrderAbstract", baseType: null, isAbstract: true, isOpen: false);
+    orderAbstract.AddKeys(orderAbstract.AddStructuralProperty("Id", EdmPrimitiveTypeKind.Int32));
+    model.AddElement(orderAbstract);
 }
 
+static void addDerivedAbstractEntityType(EdmModel model)
+{
+    EdmEntityType order = new("Lesson10", "Order", baseType: model.FindType("Lesson10.OrderAbstract") as IEdmEntityType);
+    order.AddStructuralProperty("Amount", EdmPrimitiveTypeKind.Decimal);
+    model.AddElement(order);
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 添加单实体类型
 static void addSingleEntityType(EdmModel model)
 {
     EdmEntityType singleCustomer = new("Lesson10", "SingleCustomer", baseType: null, isAbstract: false, isOpen: false);
@@ -78,17 +82,19 @@ static void addSingleEntityType(EdmModel model)
     singleCustomer.AddStructuralProperty("Name", EdmPrimitiveTypeKind.String);
     model.AddElement(singleCustomer);
 }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 添加默认容器
 static void addDefaultContainer(EdmModel model)
 {
     EdmEntityContainer container = new("Lesson10", "DefaultContainer");
     model.AddElement(container);
 }
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 添加导航属性
 static void addNavigationProperty(EdmModel model)
 {
-    EdmEntityType customer = model.FindType("Lesson10.Customer") as EdmEntityType;
-    EdmEntityType order = model.FindType("Lesson10.Order") as EdmEntityType;
+    EdmEntityType? customer = model.FindType("Lesson10.Customer") as EdmEntityType;
+    EdmEntityType? order = model.FindType("Lesson10.Order") as EdmEntityType;
     if (customer != null && order != null)
     {
         customer.AddUnidirectionalNavigation(new EdmNavigationPropertyInfo
@@ -105,23 +111,67 @@ static void addNavigationProperty(EdmModel model)
             Target = customer,
             ContainsTarget = false
         });
-
     }
 }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+static void addUnboundFunction(EdmModel model)
+{
+    EdmFunction function = new("Lesson10", "GetCustomerById", new EdmEntityTypeReference(model.FindType("Lesson10.Customer") as IEdmEntityType, true), isBound: false, entitySetPathExpression: null, isComposable: false);
+    function.AddParameter("key", EdmCoreModel.Instance.GetInt32(false));
+    model.AddElement(function);
+}
+
+static void addBoundFunction(EdmModel model)
+{
+    EdmFunction function = new("Lesson10", "GetCustomerName", EdmCoreModel.Instance.GetString(false), isBound: true, entitySetPathExpression: null, isComposable: false);
+    function.AddParameter("customer", new EdmEntityTypeReference(model.FindType("Lesson10.Customer") as IEdmEntityType, false));
+    model.AddElement(function);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+static void addUnboundAction(EdmModel model)
+{
+    EdmAction action = new("Lesson10", "UpdateCustomer", EdmCoreModel.Instance.GetString(false), isBound: false, entitySetPathExpression: null);
+    action.AddParameter("customer", new EdmEntityTypeReference(model.FindType("Lesson10.Customer") as IEdmEntityType, false));
+    model.AddElement(action);
+}
+
+static void addBoundAction(EdmModel model)
+{
+    EdmAction action = new("Lesson10", "UpdateCustomerName", EdmCoreModel.Instance.GetString(false), isBound: true, entitySetPathExpression: null);
+    action.AddParameter("customer", new EdmEntityTypeReference(model.FindType("Lesson10.Customer") as IEdmEntityType, false));
+    action.AddParameter("name", EdmCoreModel.Instance.GetString(false));
+    model.AddElement(action);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 static IEdmModel GetEdmModel()
 {
     EdmModel model = new EdmModel();
+
     addEnumType(model);
+
     addComplexType(model);
     addDerivedComplexType(model);
     addAbstractComplexType(model);
+
     addEntityType(model);
     addDerivedEntityType(model);
     addAbstractEntityType(model);
+    addDerivedAbstractEntityType(model);
+
     addSingleEntityType(model);
+
     addNavigationProperty(model);
+
+    addUnboundFunction(model);
+    addBoundFunction(model);
+
+    addUnboundAction(model);
+    addBoundAction(model);
 
     addDefaultContainer(model);
 
