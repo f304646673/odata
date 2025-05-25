3b74257211f110f7e89edaf68e8d9b3fa9380d24
fangliang <fangliang1988@gmail.com>
2025-05-25 23:10:46 +0800

    重构 BooksController 和优化 OData 配置
    在 `BooksController.cs` 中重新添加 `IncrementBookYear` 方法，确保其逻辑完整并处理模型状态。
    
    在 `Program.cs` 中调整 OData EDM 模型构建，新增 `mostRecent` 函数，并在 `incrementBookYear` 动作中添加 `id` 参数。同时优化 OData 配置顺序，提高代码可读性。

--- csharp/Lesson/Lesson9/Program.cs
+++ csharp/Lesson/Lesson9/Program.cs
@@ -11,10 +11,11 @@ static IEdmModel GetEdmModel()
 {
     var modelBuilder = new ODataConventionModelBuilder();
     modelBuilder.EntitySet<Book>("books");
+
     modelBuilder.EntityType<Book>().Collection.Function("mostRecent").Returns<string>();
     modelBuilder.Function("returnAllForKidsBooks").ReturnsFromEntitySet<Book>("books");
-    modelBuilder.EntityType<Book>() .Action("rate").Parameter<int>("rating");
 
+    modelBuilder.EntityType<Book>().Action("rate").Parameter<int>("rating");
     var action = modelBuilder.Action("incrementBookYear").ReturnsFromEntitySet<Book>("books");
     action.Parameter<int>("increment");
     action.Parameter<string>("id");
@@ -25,13 +26,7 @@ static IEdmModel GetEdmModel()
 // 添加 OData 服务和配置
 builder.Services.AddControllers().AddOData(options =>
     {
-        options.Select()
-           .Filter()
-           .OrderBy()
-           .Expand()
-           .Count()
-           .SetMaxTop(null)
-           .AddRouteComponents("odata", GetEdmModel());
+        options.AddRouteComponents("odata", GetEdmModel()).Count().OrderBy().Filter().Select().Expand();
         options.RouteOptions.EnableNonParenthesisForEmptyParameterFunction = true;
     }
     
